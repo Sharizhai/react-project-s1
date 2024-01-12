580aa36f1517b50e18bd328df901e1bf198449c7
Sharizhai <110264035+Sharizhai@users.noreply.github.com>
2024-01-12 15:30:28 +0100

    Add files via upload

--- src/Components/InformationsField.js
+++ src/Components/InformationsField.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import ButtonComp from "./ButtonComp";
 
 const InformationsField = () => {
+
+    const [selectedButtons, setSelectedButtons] = useState(Array(11).fill(false));
+
+    const handleButtonSelected = (index) => {
+        setSelectedButtons((prevSelectedButtons) => {
+            const newSelectedButtons = [...prevSelectedButtons];
+            newSelectedButtons[index] = !newSelectedButtons[index];
+
+            return newSelectedButtons;
+        });
+    };
+
     return (
         <>
-        <h4>Informations de l'annonce</h4>
+        <form>
+            <h4>Informations de l'annonce</h4>
 
-        <div>
-            <h4>Plus de détails</h4>
-            <p>Ajoutez des précisions</p>
+            <div>
+                <h4>Plus de détails</h4>
+                <p>Ajoutez des précisions</p>
 
-        </div>
+                <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap"}}>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Jantes aluminium" type="button"
+                            borderColor={selectedButtons[0] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Airbags" type="button"
+                            borderColor={selectedButtons[1] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Climatisation" type="button"
+                            borderColor={selectedButtons[2] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Système de navigation" type="button"
+                            borderColor={selectedButtons[3] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Toit ouvrant" type="button"
+                            borderColor={selectedButtons[4] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Sièges cuir" type="button"
+                            borderColor={selectedButtons[5] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Radar de recul" type="button"
+                            borderColor={selectedButtons[6] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Caméra de recul" type="button"
+                            borderColor={selectedButtons[7] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="Vitres électriques" type="button"
+                            borderColor={selectedButtons[8] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="ABS" type="button"
+                            borderColor={selectedButtons[9] ? "#89d8e0" : "black"}/>
+                    <ButtonComp onClick={() => handleButtonSelected(0)} label="ESP" type="button"
+                            borderColor={selectedButtons[10] ? "#89d8e0" : "black"}/>   
+                </div>
+            </div>
+        </form>
         </>
     );
 };
